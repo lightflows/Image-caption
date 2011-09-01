c29b3edc91271dc2ac4900f3d5a5541d79533baa
admin <admin@lightserve.private>
2011-09-01 16:08:25 +0100

    Add image caption module files

---
+++ image_caption.js
@@ -0,0 +1,21 @@
+/*$Id: image_caption.js,v 1.2.2.3 2010/02/03 07:50:25 davidwhthomas Exp $*/
+$(document).ready(function(){
+  $("img.caption").each(function(i) {
+    var imgwidth = $(this).width();
+    var imgheight = $(this).height();
+    var captiontext = $(this).attr('title');
+    var style = $(this).attr('style');
+    //var alignment = $(this).attr('align');
+    var alignment = $(this).css('float');
+    
+    //Clear image styles to prevent conflicts with parent div
+    //$(this).attr({align:""});
+    $(this).attr({style:""});
+    $(this).wrap("<span class=\"image-caption-container "+alignment+"\" style=\"display:block;" + style + "; float: " + alignment + "\"></span>");
+    $(this).parent().addClass('image-caption-container-' + alignment);
+    if(imgwidth != 'undefined' && imgwidth != 0){
+      $(this).parent().width(imgwidth);
+    }
+    $(this).parent().append("<span style=\"display:block;\" class=\"image-caption\">" + captiontext + "</span>");
+  });
+});
